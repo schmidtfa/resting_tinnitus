15be40ff04220635465519b01e506d4b8b483aa6
b1059770 <b1059770@cf000761.hpc.sbg.ac.at>
2023-10-03 15:11:32 +0200

    add headmodels local

--- notebooks/headmodels_local.py
+++ notebooks/headmodels_local.py
@@ -12,16 +12,36 @@ bad_subjects = ['19541130anfn', '19590423mrbr',
                 '19930709crgl', '19930727agwl',
                 ]
 
+local = True
+
+subject_id = bad_subjects[2]
+
+
+if local:
+    home_base = '/Users/b1059770/Library/Group Containers/G69SCX94XU.duck/Library/Application Support/duck/Volumes.noindex/bomber/resting_tinnitus'
+else:
+    home_base = '/mnt/obob/staff/fschmidt/resting_tinnitus/'
+
 #%%
-mri_path = '/mnt/obob/staff/fschmidt/resting_tinnitus/data/freesurfer'
+mri_path = join(home_base, 'data/freesurfer')
 fs_path = join(mri_path, 'fsaverage')
 trans = 'fsaverage' 
 
-df = pd.read_csv('/mnt/obob/staff/fschmidt/resting_tinnitus/data/tinnitus_match.csv').query(f'subject_id == "{subject_id}"')
+plot_kwargs = dict(subject=trans, subjects_dir=mri_path,
+                   surfaces="head-dense", dig=True, eeg=[],
+                   meg='sensors', show_axes=True,
+                   coord_frame='meg')
+view_kwargs = dict(azimuth=45, elevation=90, distance=0.6,
+                   focalpoint=(0., 0., 0.))
 
-raw = mne.io.read_raw_fif(df['path'].to_list()[0], verbose=False)
+df = pd.read_csv(join(home_base, 'data/tinnitus_match.csv')).query(f'subject_id == "{subject_id}"')
 
-max_settings_path = '/mnt/obob/staff/fschmidt/resting_tinnitus/utils'
+path_sinuhe = '/Users/b1059770/Library/Group Containers/G69SCX94XU.duck/Library/Application Support/duck/Volumes.noindex/obob'
+
+#%%
+raw = mne.io.read_raw_fif(join(path_sinuhe, df['path'].to_list()[0][1:]), verbose=False)
+
+max_settings_path = join(home_base, 'utils')
 #cal & cross talk files specific to system
 calibration_file = join(max_settings_path, 'sss_cal.dat')
 cross_talk_file = join(max_settings_path, 'ct_sparse.fif')
@@ -47,9 +67,22 @@ info = mne.pick_info(raw.info, mne.pick_types(raw.info, meg=True))
 #%% do the coregistration
 coreg = Coregistration(info, trans, mri_path)
 coreg.fit_fiducials(verbose=True)
+
+
+#%%
+fig = mne.viz.plot_alignment(info, trans=coreg.trans, **plot_kwargs) 
+fig.plotter.add_text('Subject ID: ' + subject_id, color='black')
+mne.viz.set_3d_view(fig, **view_kwargs)
+
 #%%
 coreg.fit_icp(n_iterations=6, nasion_weight=2, verbose=True)
 coreg.omit_head_shape_points(distance=15 / 1000)
+
+#%%
+fig = mne.viz.plot_alignment(info, trans=coreg.trans, **plot_kwargs) 
+fig.plotter.add_text('Subject ID: ' + subject_id, color='black')
+mne.viz.set_3d_view(fig, **view_kwargs)
+
 #%%
 coreg.fit_icp(n_iterations=20, nasion_weight=10, verbose=True)
 
@@ -57,12 +90,15 @@ dists = coreg.compute_dig_mri_distances() * 1e3  # in mm
 print(f"Distance between HSP and MRI (mean/min/max):\n{np.mean(dists):.2f} mm "f"/ {np.min(dists):.2f} mm / "
     f"{np.max(dists):.2f} mm")
 
-file = open("/mnt/obob/staff/fschmidt/resting_tinnitus/data/headmodels/" + subject_id + "/info.pickle", 'wb')
+
+#%% save
+
+file = open(join(home_base, "data/headmodels/") + subject_id + "/info.pickle", 'wb')
 pickle.dump(info, file)
 file.close()    
 
-#%%
-outpath = ''
+outpath = join(home_base, f'data/headmodels/{subject_id}/{subject_id}-trans.fif')
 
 mne.write_trans(outpath, coreg.trans, overwrite=True)
 print('Coregistration done!')
+# %%
